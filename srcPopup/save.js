487ebc6f0a2ea588a67974fe110afdbbdd6f3dc0
Antony rubombora <tonyrubombora@gmail.com>
2023-04-26 11:47:24 +0200

    com

--- srcPopup/save.js
+++ srcPopup/save.js
@@ -1,15 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     chrome.storage.local.get('moodle_key', function(items) {
         if (!chrome.runtime.error) {
-            let user = items.moodle_key.username
-            if (user) {
-                document.getElementById('title').innerHTML =
-                    'There is login information stored for <b>' + user + '</b>. ' +
-                    'If you want to change it simply enter the new login data.' +
-                    ''
+            let username = items.moodle_key.username;
+            if (username) {
+                document.getElementById('top').innerHTML = '<h1>Hey there,</h1> <b> ' + username + '</b>';
             }
         }
-    })
+    });
+
+    var dropdownBtn = document.getElementById("dropdown-btn");
+    var textDiv = document.getElementById("text");
+
+    textDiv.style.display = "none";
+    dropdownBtn.innerHTML = "Show more";
+
+    dropdownBtn.addEventListener("click", function() {
+        if (textDiv.style.display === "none") {
+            textDiv.style.display = "block";
+            dropdownBtn.innerHTML = "Show less";
+        } else {
+            textDiv.style.display = "none";
+            dropdownBtn.innerHTML = "Show more";
+        }
+    });
+
+
 
     document.querySelector('.save').addEventListener('click', onclick, false)
 
